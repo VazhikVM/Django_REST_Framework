4551b3de2e178f6a615801618378ea61e99180fd
VazhikVM <Ulenka1990>
2022-01-21 22:08:51 +0300

    lesson_5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,54 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';
+
+import './bootstrap/css/bootstrap.min.css'
+import './bootstrap/css/sticky-footer-navbar.css'
+import Footer from './components/Footer.js'
+import Navbar from './components/Menu.js'
 import UserList from './components/users.js'
 import axios from 'axios'
 
 
+const DOMAIN = 'http://127.0.0.1:8000'
+const get_url = (url) => `${DOMAIN}${url}`
+
+
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            navbarItems: [
+                {name: 'Users', href: '/'},
+            ],
+            users: []
+        }
+    }
 
-   constructor(props) {
-       super(props)
-       this.state = {
-           'users': []
-       }
-   }
+    render() {
+         return (
+            <div>
+              <header>
+              <Navbar navbarItems={this.state.navbarItems} />
+              </header>
+              <main role="main" class="flex-shrink-0">
+              <div className="container">
+                <UserList users={this.state.users} />
+              </div>
+              </main>
+              <Footer />
+            </div>
 
-componentDidMount() {
-   axios.get('http://127.0.0.1:8000/api/users/')
-       .then(response => {
-           const users = response.data
-               this.setState(
-               {
-                   'users': users
-               }
-           )
-       }).catch(error => console.log(error))
+
+            )
+    }
+
+    componentDidMount() {
+            axios.get(get_url('/api/users/'))
+        .then(response => {
+            this.setState({users: response.data})
+        }).catch(error => console.log(error))
+    }
 }
 
-   render () {
-       return (
-           <div>
-               <UserList users={this.state.users} />
-           </div>
-       )
-   }
-}
+
 export default App;
